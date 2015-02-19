d0731f0ef5f309788ce670e8cfc7970e9c01805d
d1gi <digital_boy@mail.ru>
2015-02-19 23:59:50 +0600

    not sending button POST data use case

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+
 if (isset($_POST['input'])) {
     sleep(1);
     file_put_contents('log.txt', date('Y-m-d H:i:s').': '.$_POST['input']."\n", FILE_APPEND);
-    $_SESSION['value'] = $_POST['input'];
+    $_SESSION['value']  = $_POST['input'];
+    $_SESSION['button'] = isset($_POST['button']) ? $_POST['button'] : '$_POST["button"] not set!';
     header('Location: index.php');
 }
-$value = isset($_SESSION['value']) ? $_SESSION['value'] : '';
+
+$value  = isset($_SESSION['value'])  ? $_SESSION['value']  : '';
+$button = isset($_SESSION['button']) ? $_SESSION['button'] : '';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,9 +22,8 @@ $value = isset($_SESSION['value']) ? $_SESSION['value'] : '';
     <title>Check Form for double click preventing</title>
 
     <link href="http://yastatic.net/bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet">
-		<script src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
-<!--    <script src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>-->
-<!--    <script src="http://yastatic.net/bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
+    <script src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://yastatic.net/bootstrap/2.3.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container">
@@ -27,15 +31,19 @@ $value = isset($_SESSION['value']) ? $_SESSION['value'] : '';
     if (!empty($value)) {
         echo "<h3>Previous value: {$value}</h3>";
     }
+
+    echo "<h3>Previous button name: {$button}</h3>";
+
     ?>
     <form method="post">
         <h4>Здесь можно проверить как уходит на сервер несколько запросов,
-            при этом работает HTML5 валидация "required"
+            при этом работает HTML5 валидация "required",
+            а в POST данных есть значение привязанного к самой кнопке, в частности button=form1
         </h4>
 
         <input type="text" name="input" placeholder="Форма 1" required autofocus>
         <br/>
-        <button class="btn btn-lg btn-primary" type="submit" id="form1_button">Жмём несколько раз</button>
+        <button class="btn btn-lg btn-primary" type="submit" id="form1_button" name="button" value="form1">Жмём несколько раз</button>
     </form>
 
     <br/>
@@ -44,15 +52,30 @@ $value = isset($_SESSION['value']) ? $_SESSION['value'] : '';
     <form method="post">
         <h4>Здесь применяется обработчик onclick на кнопке,
             при этом HTML5 валидация "required" не работает,
-            но нет возможности посылать несколько запросов.
+            но нет возможности посылать несколько запросов,
+            а также в POST данных нету значения привязанного к самой кнопке, в частности button=form2
         </h4>
 
         <input type="text" name="input" placeholder="Форма 2" required>
         <br/>
-        <button onclick="this.disabled=true;this.form.submit();" class="btn btn-lg btn-primary" type="submit" id="form2_button">Жмём несколько раз (с onclick)</button>
+        <button onclick="this.disabled=true;this.form.submit();" class="btn btn-lg btn-primary" type="submit" id="form2_button" name="button" value="form2">Жмём несколько раз (с onclick)</button>
     </form>
 
-    <h2>Задача: как сделать так, что бы работала и HTML5 валидация и блокировка от нескольких кликов?</h2>
+    <br/>
+    <br/>
+
+    <form onsubmit="document.getElementById('form3_button').disabled=true;" id="form3" method="post">
+        <h4>Здесь применяется обработчик onsubmit на форме,
+            при этом HTML5 валидация "required" работает и нет возможности посылать несколько запросов.
+            <br/> НО в POST данных нету значения привязанного к самой кнопке, в частности button=form3
+        </h4>
+
+        <input type="text" name="input" placeholder="Форма 3" required>
+        <br/>
+        <button class="btn btn-lg btn-primary" type="submit" id="form3_button" name="button" value="form3">Жмём несколько раз</button>
+    </form>
+
+    <h2>Задача: как сделать так, что бы работала и HTML5 валидация, и блокировка от нескольких кликов, и уходили POST данные самой кнопки?</h2>
 
     <p>Проверил на 2-х браузерах:</p>
     <ul>
@@ -71,24 +94,14 @@ $value = isset($_SESSION['value']) ? $_SESSION['value'] : '';
                 </li>
             </ul>
         </li>
+        <li>
+            Форма 3 отлично справляется с валидацией и предотвращением множественных отправок в любых браузерах, но не посылает POST данные кнопки.
+        </li>
     </ul>
 
-		<br/>
-		<br/>
+    <br/>
+    <br/>
 
-		<h2>Может так?</h2>
-		<p>используется jquery</p>
-
-		<form id="form3" method="post">
-				<input type="text" name="input" placeholder="Форма 3" required>
-				<br/>
-				<button class="btn btn-lg btn-primary" type="submit" id="form3_button">Жмём несколько раз</button>
-				<script type="text/javascript">
-					$('#form3').submit(function(){
-						$('#form3_button').attr('disabled', 'disabled');
-					});
-				</script>
-		</form>
 </div>
 
 </body>
